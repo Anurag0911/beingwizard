2cd3dc1242d49394f99a3cede154d71907102df8
Anurag Sharma <anurag.asy81@gmail.com>
2021-05-29 15:10:49 +0530

    User sigin token done

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
-// for the structure 
-const User = require('../models/user');
+
+const User = require('../models/user'); // for the structure 
 
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
@@ -24,6 +24,44 @@ exports.signup = (req, res) => {
         });
     });
 };  
+
+
+
+
+
+const jwt = require("jsonwebtoken"); // this is to generate signed token 
+const expressjwt = require("expressjwt"); // for authorization check 
+
+
+exports.signin= (req,res)=>{
+    // find the user based on Email
+    const {email, password}= req.body
+    User.findOne({email},(err, user)=>{       // findone funtion us to find the email from the database
+        if ( err || !user ){
+            return res.status(400).json({
+                err : "User with this email does not exist please signup"
+            });
+        }   
+        // if the user is found than lets authorize
+        
+        
+        //generate a signed token for user id and secerat
+        const token = jwt.sign({_id: user.id},process.env.JWT_SECRET);
+        // persist the token as "tok" in cookie with expiray date 
+        res.cookie('tok',token, {expire: new Date() + 14400});
+        // return response with user and token to frontend client 
+        const {_id, name, email, role }= user;   // using desstruct to ease the sending of the user
+
+        return res.json({token,  user:{_id, name, email, role }});
+
+
+
+
+
+    })
+
+
+};
 
 
 
